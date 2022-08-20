a61bf9675099e20d43f8d36eabb2b6485fd09a54
Olivia Conley <oliviarconley@gmail.com>
2022-08-20 15:14:04 -0400

    New JS on genre button population

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,34 @@
-var fakedata = {
-    gen: ["rock", "pop", "Indie"],
-    music: ["song1"]
+var quizIntroPage = document.querySelector(".quiz-intro flex content-center p-4 h-screen"); 
+var startBtn = document.querySelector("#start-button");
+var genreSelectPage = document.querySelector(".select-genre max-w-screen-lg mx-auto h-screen"); 
+
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    quizIntroPage.style.display = 'none';
+    genreSelectPage.style.display = 'block'; 
+    randomGenreGenerator()
 }
 
-console.log(fakedata.gen[0]);
 
-fakedata.gen.forEach(element => {
-    // create the button element
-    // style it
-    //attach to parent div
-});
 
-//FOR TODAY: Work on the fetch to pull data...
+//FOR GENRE SELECTION. So we need the data to populate the different genres
+var mockData = ["Rock", "Pop", "Hip Hop", "R&B", "Indie", "Country"] //should have the genre select array here 
+
+function randomGenreGenerator() {
+    //var genreDelete = [Math.floor(Math.random()*mockData.length)] // gets random item from array 
+    //save the info in the square brackets into variable [Math.floor(Math.random()*mockData.length)]
+    //pass random number into the mockData index 
+    //delete specific item from array. so once it populates a button it removes that info from the array. 
+    for (var i = 0; i < 4; i++)  {
+        console.log($(`#${i}`))
+        $(`#${i}`).text(mockData[Math.floor(Math.random()*mockData.length)])
+    }
+}
+
+//FOR  BUTTON CREATION
+// fakedata.gen.forEach(element => {
+//     // create the button element
+//     // style it
+//     //attach to parent div
+// });
